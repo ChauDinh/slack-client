47a8153beaa8426134b1860625a327c2fd749d1f
Chau Dinh <chaudvb95uc@gmail.com>
2019-08-02 12:08:58 +0700

    Switching routes when click on username in direct message user list

--- src/components/DirectMessageModal.js
+++ src/components/DirectMessageModal.js
@@ -1,79 +1,88 @@
 import React from "react";
 import { Form, Input, Button, Modal } from "semantic-ui-react";
 import Downshift from "downshift";
-import { render } from "react-dom";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
+import { withRouter } from "react-router-dom";
 
-const items = [
-  { value: "apple" },
-  { value: "pear" },
-  { value: "orange" },
-  { value: "grape" },
-  { value: "banana" }
-];
+const getTeamMembersQuery = gql`
+  query($teamId: Int!) {
+    getTeamMembers(teamId: $teamId) {
+      id
+      username
+    }
+  }
+`;
 
-const DirectMessageModal = ({ open, onClose, teamId }) => (
+const DirectMessageModal = ({
+  history,
+  open,
+  onClose,
+  teamId,
+  data: { loading, getTeamMembers }
+}) => (
   <Modal open={open} onClose={onClose}>
     <Modal.Header>Direct Messages</Modal.Header>
     <Modal.Content>
       <Form>
         <Form.Field>
           {/* <Input name="name" fluid placeholder="Search users ..." /> */}
-          <Downshift
-            onChange={selection =>
-              alert(
-                selection
-                  ? `You selected ${selection.value}`
-                  : "Selection Cleared"
-              )
-            }
-            itemToString={item => (item ? item.value : "")}
-          >
-            {({
-              getInputProps,
-              getItemProps,
-              getLabelProps,
-              getMenuProps,
-              isOpen,
-              inputValue,
-              highlightedIndex,
-              selectedItem
-            }) => (
-              <div>
-                <label {...getLabelProps()}>Please choose an user</label>
-                <Input
-                  {...getInputProps({ placeholder: "favorite user" })}
-                  fluid
-                />
-                <ul {...getMenuProps()}>
-                  {isOpen
-                    ? items
-                        .filter(
-                          item => !inputValue || item.value.includes(inputValue)
-                        )
-                        .map((item, index) => (
-                          <li
-                            {...getItemProps({
-                              key: item.value,
-                              index,
-                              item,
-                              style: {
-                                backgroundColor:
-                                  highlightedIndex === index
-                                    ? "lightgray"
-                                    : "white",
-                                fontWeight:
-                                  selectedItem === item ? "bold" : "normal"
-                              }
-                            })}
-                          >
-                            {item.value}
-                          </li>
-                        ))
-                    : null}
-                </ul>
-              </div>
-            )}
-          </Downshift>
+          {!loading && (
+            <Downshift
+              onChange={selectedUser => {
+                history.push(`/view-team/user/${teamId}/${selectedUser.id}`);
+                onClose();
+              }}
+              itemToString={item => (item ? item.value : "")}
+            >
+              {({
+                getInputProps,
+                getItemProps,
+                getLabelProps,
+                getMenuProps,
+                isOpen,
+                inputValue,
+                highlightedIndex,
+                selectedItem
+              }) => (
+                <div>
+                  <label {...getLabelProps()}>Please choose an user</label>
+                  <Input
+                    {...getInputProps({ placeholder: "favorite user" })}
+                    fluid
+                  />
+                  <ul {...getMenuProps()} style={{ listStyle: "none" }}>
+                    {isOpen
+                      ? getTeamMembers
+                          .filter(
+                            item =>
+                              !inputValue || item.username.includes(inputValue)
+                          )
+                          .map((item, index) => (
+                            <li
+                              {...getItemProps({
+                                key: item.id,
+                                // index,
+                                item,
+                                style: {
+                                  backgroundColor:
+                                    highlightedIndex === index
+                                      ? "lightgray"
+                                      : "white",
+                                  fontWeight:
+                                    selectedItem === item ? "bold" : "normal"
+                                }
+                              })}
+                            >
+                              {item.username}
+                            </li>
+                          ))
+                      : null}
+                  </ul>
+                </div>
+              )}
+            </Downshift>
+          )}
         </Form.Field>
         <Form.Group widths="equal">
           <Button onClick={onClose} fluid>
@@ -85,4 +94,4 @@ const DirectMessageModal = ({ open, onClose, teamId }) => (
   </Modal>
 );
 
-export default DirectMessageModal;
+export default withRouter(graphql(getTeamMembersQuery)(DirectMessageModal));
