4a790c41177b6b21cb40a7c0b7ed6cf0da8f3b55
Chau Dinh <chaudvb95uc@gmail.com>
2020-04-25 13:26:08 +0700

    ui: update userModal component

--- src/components/UserModal.js
+++ src/components/UserModal.js
@@ -1,10 +1,34 @@
 import React from "react";
 import { Button, Header, Image, Modal, Icon } from "semantic-ui-react";
+import styled from "styled-components";
 
 import OnlineUser from "./OnlineUser";
 import "./UserModal.css";
 
-const UserModal = ({ name, last_seen }) => (
+const ButtonStyle = styled.button`
+  width: 45px;
+  height: 45px;
+  background-color: #cccccc;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  &:hover {
+    background-color: "green";
+    cursor: pointer;
+  }
+`;
+
+const ParaStyle = styled.p`
+  padding: 15px 15px;
+  margin-bottom: 10px;
+  border: 1px solid rgba(200, 200, 200, 0.3);
+  border-radius: 5px;
+  background-color: #fff;
+`;
+
+const UserModal = ({ name }) => (
   <Modal
     trigger={
       <Button
@@ -16,7 +40,7 @@ const UserModal = ({ name, last_seen }) => (
         }}
       >
         {" "}
-        <OnlineUser name={name} last_seen={last_seen} />
+        <OnlineUser name={name} />
       </Button>
     }
     centered={false}
@@ -25,8 +49,40 @@ const UserModal = ({ name, last_seen }) => (
       maxWidth: "450px",
     }}
   >
-    <Modal.Header style={{ backgroundColor: "#3f9fff", color: "#fff" }}>
-      User info
+    <Modal.Header
+      style={{
+        padding: 0,
+        backgroundColor: "transparent",
+        boxShadow: "0px 3px 5px rgba(200, 200, 200, 0.2)",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "#3f9fff",
+          color: "#fff",
+          fontSize: 15,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "3px 3px 0 0",
+          borderBottom: "1px solid rgba(200, 200, 200, 0.5)",
+        }}
+      >
+        SlackQL
+      </div>
+      <div
+        style={{
+          padding: "15px 0 10px 0",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          fontFamily: "AvenirNext",
+          color: "#333",
+          fontWeight: "900",
+        }}
+      >
+        {name}'s profile
+      </div>
     </Modal.Header>
     <Modal.Content
       image
@@ -36,45 +92,56 @@ const UserModal = ({ name, last_seen }) => (
         backgroundColor: "#f4f7fa",
       }}
     >
-      <Image
-        wrapped
-        className="user_avatar"
-        size="medium"
-        src={`https://api.adorable.io/avatars/100/${name}@adorable.io`}
+      <div
         style={{
-          width: "100%",
           display: "flex",
-          justifyContent: "center",
           alignItems: "center",
+          justifyContent: "center",
+          padding: "0px 40px",
         }}
-      />
+      >
+        <ButtonStyle>
+          <Icon name="call" size="large" />
+        </ButtonStyle>
+        <Image
+          wrapped
+          className="user_avatar"
+          size="medium"
+          src={`https://api.adorable.io/avatars/100/${name}@adorable.io`}
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        />
+        <ButtonStyle>
+          <Icon name="talk" size="large" />
+        </ButtonStyle>
+      </div>
       <Modal.Description style={{ paddingLeft: "0", fontFamily: "sans-serif" }}>
         <Header style={{ textAlign: "center", marginTop: "10px" }}>
           {name}
         </Header>
-        <p>
-          <Icon name="clock outline" />
-          <span style={{ fontWeight: "bolder" }}>Last seen:</span>{" "}
-          {Date(`${last_seen}`)}
-        </p>
-        <p>
+
+        <ParaStyle>
           <Icon name="mail outline" />
-          <span style={{ fontWeight: "bolder" }}>Email adress:</span> {name}@
+          <span style={{ fontWeight: "bolder" }}>Email address:</span> {name}@
           {name}.com
-        </p>
-        <p>
+        </ParaStyle>
+        <ParaStyle>
           <Icon name="text telephone" />
           <span style={{ fontWeight: "bolder" }}>Phone:</span> +84 389 139 3849
-        </p>
-        <p>
+        </ParaStyle>
+        <ParaStyle>
           <Icon name="at" />
-          <span style={{ fontWeight: "bolder" }}>Adress:</span> MO 2, United
+          <span style={{ fontWeight: "bolder" }}>Address:</span> MO 2, United
           State
-        </p>
-        <p>
+        </ParaStyle>
+        <ParaStyle>
           <Icon name="credit card outline" />
           <span style={{ fontWeight: "bolder" }}>Position:</span> Developer
-        </p>
+        </ParaStyle>
       </Modal.Description>
     </Modal.Content>
   </Modal>
